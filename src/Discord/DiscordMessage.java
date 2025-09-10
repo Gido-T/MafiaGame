d46c34e81973aa1643d1c56e60f48194b5ddd8fc
Gido-T <113729656+Gido-T@users.noreply.github.com>
2025-09-10 10:38:51 -0500

    completed adding stuff to trait enum

--- src/Discord/DiscordMessage.java
+++ src/Discord/DiscordMessage.java
@@ -6,5 +6,7 @@ public class DiscordMessage {
 
     private String message;
 
-    private 
+    public class Builder {
+        
+    }
 }
--- src/Enums/Trait.java
+++ src/Enums/Trait.java
@@ -1,6 +1,12 @@
 public enum Trait {
     NOCTURNAL("Nocturnal", "This role does something at night."),
-    EXECUTIONER("Executioner", "This role is capable of killing without voting.");
+    EXECUTIONER("Executioner", "This role is capable of killing without voting."),
+    INVESTIGATOR("Investigator","This role learns information about other players."),
+    SUBTLE("Subtle","This role’s ability does not trigger other roles, such as Night-Watch or Winderghiest."),
+    HEAD_START("Head-Start","This role gains some benefit at the start of the game."),
+    DEFENDER("Defender","This role does something in response to another role targeting them or this role’s target."),
+    ALIBI("Alibi","This role is capable of revealing itself to the other players, confirming their role."),
+    COPYCAT("Copycat","This role utilizes the abilities of other roles.");
 
     private final String name;
     private final String description;
@@ -9,4 +15,10 @@ public enum Trait {
         this.name = name;
         this.description = description;
     }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+
+    public boolean eqauls(String str) { return str.equals(this.name); }
+    public boolean eqauls(Trait trait) { return equals(trait.getName()); }
 }
